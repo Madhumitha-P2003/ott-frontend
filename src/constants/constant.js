224497a6612f9647ad52a0553916377bf9dc7b12
Madhumitha Perumal <sugunavathisugunavathi423@gmail.com>
2026-01-22 14:42:02 +0530

    Update constants configuration

--- src/constants/constant.js
+++ src/constants/constant.js
@@ -1,3 +1,5 @@
+//constants config
+
 export const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
 export const IMG_BASE = "https://image.tmdb.org/t/p/w500";
 export const BASE_URL = "https://api.themoviedb.org/3";

7e4907213683a291377d904cc975d4430363b30c
Madhumitha Perumal <sugunavathisugunavathi423@gmail.com>
2026-01-22 14:37:28 +0530

    Finalize env-based API key and Axios config

--- src/constants/constant.js
+++ src/constants/constant.js
@@ -1,5 +1,3 @@
 export const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
-
-export const BaseUrl = "https://api.themoviedb.org/3";
-
 export const IMG_BASE = "https://image.tmdb.org/t/p/w500";
+export const BASE_URL = "https://api.themoviedb.org/3";
